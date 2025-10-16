083803d1758a5c1579336c0a616c8e17f9f97896
Reece Van Atta <reece@thrasker.com>
2025-10-16 17:59:41 -0400

    Add initial dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,36 @@
+import { createEffect } from "solid-js";
+
 export default function App() {
-	return <div>Hello</div>;
+	let dialog!: HTMLDialogElement;
+
+	createEffect(() => {
+		dialog.showModal();
+	});
+
+	let form!: HTMLFormElement;
+
+	function onSubmit() {
+		console.log(new FormData(form));
+	}
+
+	return (
+		<>
+			<dialog class="modal" ref={dialog}>
+				<div class="modal-box">
+					<h2 class="text-xl capitalize font-bold">Start new task</h2>
+					<form method="dialog" ref={form} on:submit={onSubmit}>
+						<fieldset class="fieldset">
+							<legend class="fieldset-legend">Name</legend>
+							<input type="text" name="name" class="input" />
+						</fieldset>
+						<div class="modal-action">
+							<button type="submit" class="btn">
+								Start
+							</button>
+						</div>
+					</form>
+				</div>
+			</dialog>
+		</>
+	);
 }
