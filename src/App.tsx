2ba4062f07224f62833660633f9d7684b91c90b5
Reece Van Atta <reece@thrasker.com>
2025-10-16 18:14:50 -0400

    Add countup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
-import { createEffect } from "solid-js";
+import { createEffect, createSignal, createMemo, Show } from "solid-js";
+import * as v from "valibot";
+
+const taskSchema = v.object({
+	start: v.instance(Temporal.Instant),
+	name: v.string(),
+});
+type Task = v.InferOutput<typeof taskSchema>;
 
 export default function App() {
 	let dialog!: HTMLDialogElement;
 
+	const [task, setTask] = createSignal<Task | undefined>({
+		name: "Foobar",
+		start: Temporal.Now.instant(),
+	});
+
 	createEffect(() => {
-		dialog.showModal();
+		if (!task()) {
+			dialog.showModal();
+		}
 	});
 
 	let form!: HTMLFormElement;
 
 	function onSubmit() {
-		console.log(new FormData(form));
+		const formData = new FormData(form);
+
+		setTask(
+			v.parse(taskSchema, {
+				start: Temporal.Now.instant(),
+				name: formData.get("name") ?? "",
+			}),
+		);
 	}
 
 	return (
@@ -31,6 +52,72 @@ export default function App() {
 					</form>
 				</div>
 			</dialog>
+			<div class="flex h-full items-center justify-center">
+				<Show when={task()}>
+					<div class="stats">
+						<div class="stat">
+							<div class="stat-title">Name</div>
+							<div class="stat-value">{task()?.name}</div>
+							<div class="stat-actions">
+								<button
+									type="button"
+									class="btn btn-primary btn-xs btn-outline"
+								>
+									Finish
+								</button>
+							</div>
+						</div>
+						<div class="stat">
+							<span class="stat-title">Duration</span>
+							<div class="stat-value">
+								<Countup start={task()!.start} />
+							</div>
+							<span class="stat-description">
+								Started at{" "}
+								{task()?.start.toLocaleString(undefined, {
+									timeStyle: "short",
+								})}
+							</span>
+						</div>
+					</div>
+				</Show>
+			</div>
 		</>
+	);
+}
+
+type CountupProps = {
+	start: Temporal.Instant;
+};
+
+function Countup(props: CountupProps) {
+	const [now, setNow] = createSignal(Temporal.Now.instant());
+	createEffect(() => {
+		setInterval(() => setNow(Temporal.Now.instant()), 1000);
+	});
+
+	const duration = createMemo(() =>
+		props.start
+			.until(now())
+			.round({ largestUnit: "hours", smallestUnit: "seconds" }),
+	);
+	const hours = () => duration()?.hours ?? 0;
+	const minutes = () => duration()?.minutes ?? 0;
+	const seconds = () => duration()?.seconds ?? 0;
+	return (
+		<span class="countdown font-mono">
+			<span class="[--digits:2]" style={{ "--value": hours() }}>
+				{hours()}
+			</span>
+			h
+			<span class="[--digits:2]" style={{ "--value": minutes() }}>
+				{minutes()}
+			</span>
+			m
+			<span class="[--digits:2]" style={{ "--value": seconds() }}>
+				{seconds()}
+			</span>
+			s
+		</span>
 	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@
 import { render } from "solid-js/web";
 import App from "./App.tsx";
 
+if (!("Temporal" in globalThis)) {
+	import("temporal-polyfill/global");
+}
+
 const root = document.getElementById("root");
 
 render(() => <App />, root!);
